c2cc1d977240c9383cb37ea8e4ec9e55007c2e02
André Domingues <dominguesstroppa@gmail.com>
2023-10-31 10:03:03 -0300

    [ADD] Componente de registro de usuario

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -3,26 +3,40 @@
 namespace App\Livewire\Auth;
 
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Register extends Component
 {
+    #[Rule(['required', 'max:255'])]
     public ?string $name;
 
+    #[Rule(['required', 'email', 'confirmed'])]
     public ?string $email;
 
+    #[Rule(['required'])]
     public ?string $email_confirmation;
 
+    #[Rule(['required'])]
     public ?string $password;
 
     public function register(): void
     {
-        User::create([
+        $this->validate();
+
+        $user = User::create([
             'name' => $this->name,
             'email' => $this->email,
             'password' => $this->password
         ]);
+
+        Auth::login($user);
+
+        $this->redirect(RouteServiceProvider::HOME);
     }
 
     public function render(): View
--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Auth\Register;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Livewire;
 
 use function Pest\Laravel\assertDatabaseCount;
@@ -22,7 +23,8 @@ test('O componente deve conseguir cadastrar um novo user no sistema', function()
         ->set('email_confirmation', 'andre@domingues.com')
         ->set('password', 'password')
         ->call('register')
-        ->assertHasNoErrors('/');
+        ->assertHasNoErrors('/')
+        ->assertRedirect('/');
 
 
     assertDatabaseHas('users', [
@@ -32,5 +34,23 @@ test('O componente deve conseguir cadastrar um novo user no sistema', function()
 
     assertDatabaseCount('users', 1);
 
+    expect(Auth::user()->name)->toBe('André');
+
 
 });
+
+test('O componente deve validar os campos obrigatórios', function($data) {
+
+    Livewire::test(Register::class)
+       ->set($data->field, $data->value)
+       ->call('register')
+       ->assertHasErrors($data->field);
+
+})->with([ //Dataset para testar os campos obrigatórios, field é o campo, value é o valor que será testado e rule é a regra que será testada
+    'name::required' => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255' => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    'email::required' => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email' => (object)['field' => 'email', 'value' => 'andre', 'rule' => 'email'],
+    'email::confirmed' => (object)['field' => 'email', 'value' => 'andre@gmail.com', 'rule' => 'confirmed'],
+    'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+]);
